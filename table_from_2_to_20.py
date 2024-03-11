e713d94716df852c43650d42aa96a415e16e1f6d
Abhijit Kurane <kuraneabhijit@gmail.com>
2024-03-11 17:10:03 +0530

    table program using range

---
+++ table_from_2_to_20.py
@@ -0,0 +1,7 @@
+for table in range(2,21,1):
+    print('')
+    print('-------Table of',table,'-------') 
+    print('')
+    for a in range(1,11,1):
+        print(table,'x',a, '=', table*a)
+        print('-'*14)
