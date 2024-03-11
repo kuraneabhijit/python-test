3b708374f383fa83ca7ff4acca6e32db0958d796
Abhijit Kurane <kuraneabhijit@gmail.com>
2024-03-11 18:13:47 +0530

    comprehensive list

--- comprehensive_list.py
+++ comprehensive_list.py
@@ -1,2 +1,7 @@
+# prints value from 1 to 100
 sample_list=[num for num in range(1,101)]
+print(sample_list)
+
+# prints value from 1 to 100 excluding number divisible by 4
+sample_list=[num for num in range(1,101) if num%4!=0]
 print(sample_list)
